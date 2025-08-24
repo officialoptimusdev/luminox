b3e901300059b9e3cf2cbfc87300c09569d215f5
Daniel Effiong <it.dannybizman@gmail.com>
2025-08-24 02:35:36 +0100

    feature: completed about page

--- src/pages/about/index.jsx
+++ src/pages/about/index.jsx
@@ -1,9 +1,15 @@
 import AboutHero from "@/components/AboutHero";
+import BrandsSection from "@/components/BrandsSection";
+import FeaturedAbout from "@/components/FeaturedAbout";
+import MeetTheTeam from "@/components/MeetTheTeam";
 
 const About = () => {
    return (
     <main className="w-full">
        <AboutHero />
+       <FeaturedAbout />
+       <BrandsSection />
+       <MeetTheTeam />
        </main>
    );
  };
