f81950889020fcefcbcd2f42e7311a66a3cee54f
Daniel Effiong <it.dannybizman@gmail.com>
2025-09-15 19:25:47 +0100

    fix: added wordpress and .github  config

--- index.php
+++ index.php
@@ -1,19 +1,21 @@
 <?php
 /* 
- * Theme bootstrapper: loads the React build
- */
-?><!DOCTYPE html>
+Theme Name: Luminox React
+Theme URI: https://luminoxmentalhealth.com
+Author: Luminox
+Description: React Vite app as a WordPress theme
+Version: 1.0
+*/
+?>
+<!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
-  <meta charset="<?php bloginfo( 'charset' ); ?>">
+  <meta charset="<?php bloginfo('charset'); ?>">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
-  <?php
-    // Output the built React index.html
-    readfile( get_template_directory() . '/index.html' );
-  ?>
+  <div id="root"></div>
   <?php wp_footer(); ?>
 </body>
 </html>
